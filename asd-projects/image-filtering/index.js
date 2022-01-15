7a2e5e1609b82fd0c372983558d1face52fe7c72
frlumii <ilymochyi@gmail.com>
2022-01-15 00:34:53 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,43 +20,14 @@ applyFilter(reddify);
 function applyFilter() {
     for (var i = 0; i < image; i++){
     } 
-}
 
-var rgbNumbers 
-
-rgbStringToArray(rgbString); [
-
-]
-filterFunction(rgbNumbers);
-// TODO 6: Create the applyFilterNoBackground function
-
-
-// TODO 3 & 5: Create filter functions
-
-function reddify();{
-    [red,255];
-}
-
-    reddify(BLUE);{
-    }
-    reddify(GREEN);{
-
-    }
-reddify(applyFilter);{
-    
-}
 function keepInBounds(boundColors) {
-    if (boundColors < 0)
  return  0;
- if (boundColors > 255)
+    }
  return 255
- if (boundColors > 0 < 255)
+    }
  return boundColors
-}
 
 
-decreaseBlue = keepInBounds(Blue - 50);
-
-increaseGreenByBlue = keepInBounds(BLUE + GREEN);
 
 // CHALLENGE code goes below here
